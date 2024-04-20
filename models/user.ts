3a2d06723aa2b849e0d01562acfcd0af29400100
Parth Agrawal <pvagrawal0@gmail.com>
2024-04-20 13:38:22 -0400

    added prisma client

---
+++ models/user.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client';
+
+export function createUser ({
+    name,
+    email,
+    password,
+    address,
+    bio,
+    id
+}:
+{
+    name:string,
+    email: string,
+    password: string,
+    address: string,
+    bio: string,
+    id: string
+
+}){
+    
+    
+    
+
+} 
