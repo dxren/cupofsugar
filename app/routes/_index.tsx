46d863d4def6c9747d6f636661e14cfa6d3ce937
Parth Agrawal <pvagrawal0@gmail.com>
2024-04-20 17:06:58 -0400

    use-state stuff

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
+import { Item } from "@prisma/client";
 import type { MetaFunction } from "@remix-run/node";
+import { useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,10 +9,32 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+interface ItemProps {
+  item: Item
+  color: string
+}
+
+function ItemCard(props: ItemProps) {
+  return (
+    <div key={props.item.id}>
+      I am an Item: {props.item.title} and my color is {props.color}
+    </div>
+  )
+}
+
+function itemToItemCard(item: Item): JSX.Element {
+  return <ItemCard item={item} color="red" />
+}
+
+
+
 export default function Index() {
+  const [itemList, setItemList] = useState<Item[]>([])
+  const [counter, updateounter] = useState(0)
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Cup of Sugar</h1>
+      <h1>Cup of Sugar: {counter} </h1>
+      
       <ul>
         <li>
           <a
@@ -30,6 +54,40 @@ export default function Index() {
             Deep Dive Jokes App Tutorial
           </a>
         </li>
+
+        <div>
+          <button onClick={async () => {
+            updateounter(counter +1)
+            
+          }}>
+            update counter
+          </button>
+
+          <button onClick={async () => {
+            // const response = await fetch('http://localhost:5174/item?user_id=001a');
+            // const data: Item[] = await response.json();
+            // console.log(data)
+
+            const newItem = {
+              id: "00122",
+              title: "new item",
+              description: "newness",
+              tag: ["default tag"],
+              available: true,
+              imageUrl: null,
+              userId: "001a",
+            }
+
+            setItemList([...itemList, newItem])
+
+          }}
+          >
+            add item
+          </button>
+          </div>
+
+
+        {itemList.map(itemToItemCard)}
         <li>
           <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
             Remix Docs
