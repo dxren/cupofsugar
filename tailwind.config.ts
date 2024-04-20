37ff60b60c365b713841feb668967631c6068079
Callan Rogers <152415776+callan-rg@users.noreply.github.com>
2024-04-20 15:31:59 -0400

    grid technically exists

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
-  content: [],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-} satisfies Config
-
+  plugins: [require('daisyui'), require('tailwindcss-react-aria-components')]
+  ,
+  daisyui: {
+    themes: ["light", "dark", "cupcake"]  },
+} satisfies Config;
